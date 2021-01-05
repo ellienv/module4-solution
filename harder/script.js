41e833e71ff8e8b796e9ecd5bab304dc42f769ad
Olena Voluychyk <alena@Air-Olena.Dlink>
2021-01-05 12:46:27 +0200

    solution for module4

--- harder/script.js
+++ harder/script.js
@@ -27,7 +27,6 @@ Good Bye Jim
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
-    console.log(firstLetter);
     if (firstLetter === "j") {
       byeSpeaker.speak(names[i]);
     } else {
